a9fbe63107f8679871a314164c02a39e4e892235
uestcjerry <jerrychenzhiyu@163.com>
2016-06-02 15:34:51 +0800

    finish kendall part

--- src/kendallCpp/Kendall.cpp
+++ src/kendallCpp/Kendall.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <cmath>
+#include <ctgmath>
 
 /*
  *	来源：	http://www.xuebuyuan.com/840043.html
@@ -79,7 +81,8 @@ bool KendallAnalysis::judgeTwoSrcFile(const std::string &srcFileOne, const std::
  *	一致性：
  *	(x_i > x_j && y_i > y_j) || (x_i < x_j && y_i < y_j)
  */
-bool KendallAnalysis::kendallExpressionOne(const std::string &srcFileOne, const std::string &srcFileTwo, const std::string &tarFileName)
+bool KendallAnalysis::kendallExpressionOne(const std::string &srcFileOne, const std::string &srcFileTwo
+						, const std::string &tarFileName)
 {
 	const std::string srcFile1 = srcFilePref + srcFileOne;
 	const std::string srcFile2 = srcFilePref + srcFileTwo;
@@ -117,6 +120,7 @@ bool KendallAnalysis::kendallExpressionOne(const std::string &srcFileOne, const 
 	///////////////////////////////////////////////////////////////
 
 	totalNodeNum = fileOneVec.size();
+
 	unsigned vecSize = fileOneVec.size();
 	for (unsigned i = 0; i < vecSize; ++i)
 		for (unsigned j = i + 1; j < vecSize; ++j) {
@@ -144,16 +148,19 @@ bool KendallAnalysis::kendallExpressionOne(const std::string &srcFileOne, const 
 	outputFile.close();
 
 	return true;
-}
+}	
 
 /*
  *	kendall 公式二
  *	T = (C - D) / (sqrt(N3 - N1) * sqrt(N3 - N2))
  *	N3 = (1/2) * N * (N - 1), N1 = sum(i = 1..s) (1/2)*Ui*(Ui - 1), N2 = sum(i = 1..t) (1/2)*Vi*(Vi - 1)
+ *	Ui 和 Vi 是重叠集合的大小
+ *
  *	注意：
  *	这一个公式适用于集合X 或者 Y 中存在相同元素的情况，当X 或者Y 之中均不存在相同元素时，公式二等于公式一
  */
-bool KendallAnalysis::kendallExpressionTwo(const std::string &srcFileOne, const std::string &srcFileTwo, const std::string &tarFileName)
+bool KendallAnalysis::kendallExpressionTwo(const std::string &srcFileOne, const std::string &srcFileTwo
+					, const std::string &tarFileName)
 {
 	const std::string srcFile1 = srcFilePref + srcFileOne;
 	const std::string srcFile2 = srcFilePref + srcFileTwo;
@@ -165,8 +172,97 @@ bool KendallAnalysis::kendallExpressionTwo(const std::string &srcFileOne, const 
 		return false;
 	}
 
-	// continue..
+	////////////////////////////////////////////////////
 
+	unsigned totalNodeNum = 0;
+	unsigned commonPairNum = 0, unCommonPariNum = 0;				// C and D
+
+	std::vector<unsigned> fileOneVec;
+	std::vector<unsigned> fileTwoVec;
+
+	std::fstream inputFile1(srcFile1, std::ios_base::in);
+	if (!inputFile1.is_open())
+		return false;
+	unsigned temp;
+	while (inputFile1 >> temp)
+		fileOneVec.push_back(temp);
+	inputFile1.close();
+
+	std::fstream inputFile2(srcFile2, std::ios_base::in);
+	if (!inputFile2.is_open())
+		return false;
+	while (inputFile2 >> temp)
+		fileTwoVec.push_back(temp);
+	inputFile2.close();
+
+	///////////////////////////////////////////////////////////////
+
+	totalNodeNum = fileOneVec.size();
+
+	unsigned vecSize = fileOneVec.size();
+	for (unsigned i = 0; i < vecSize; ++i)
+		for (unsigned j = i + 1; j < vecSize; ++j) {
+			if (fileOneVec.at(i) > fileOneVec.at(j) && fileTwoVec.at(i) > fileTwoVec.at(j))
+				commonPairNum++;
+			else if (fileOneVec.at(i) < fileOneVec.at(j) && fileTwoVec.at(i) < fileTwoVec.at(j))
+				commonPairNum++;
+			else
+				unCommonPariNum++;
+		}
+
+
+	//////////////////////////////////////////////////
+
+	unsigned maxNodeInOne = 0, maxNodeInTwo = 0;
+	for (const auto &elem : fileOneVec)
+		if (elem > maxNodeInOne)
+			maxNodeInOne = elem;
+	for (const auto &elem : fileTwoVec)
+		if (elem > maxNodeInTwo)
+			maxNodeInTwo = elem;
+
+	//////////////////////////////////////////////////
+
+	std::vector<unsigned> timeCountOneVec = std::vector<unsigned>(maxNodeInOne + 1);
+	std::vector<unsigned> timeCountTwoVec = std::vector<unsigned>(maxNodeInTwo + 1);
+
+	for (const auto &elem : fileOneVec)
+		timeCountOneVec.at(elem)++;
+	for (const auto &elem : fileTwoVec)
+		timeCountTwoVec.at(elem)++;
+
+	/////////////////////////////////////////////////
+
+	double N3 = 0.5 * static_cast<double>(totalNodeNum * (totalNodeNum - 1));
+	double N1 = 0, N2 = 0;
+
+	for (unsigned i = 1; i <= maxNodeInOne; ++i) {
+		unsigned temp = timeCountOneVec.at(i);
+		if (temp > 1)
+			N1 += 0.5 * static_cast<double>(temp * (temp - 1));
+	}
+			
+	for (unsigned j = 1; j <= maxNodeInTwo; ++j) {
+		unsigned temp = timeCountTwoVec.at(j);
+		if (temp > 1)
+			N2 += 0.5 * static_cast<double>(temp * (temp - 1));
+	}
+	
+	//	T = (C - D) / (sqrt(N3 - N1) * sqrt(N3 - N2))
+	double kendall = static_cast<double>(commonPairNum - unCommonPariNum) / 
+		(sqrt(static_cast<double>(N3 - N1)) * sqrt(static_cast<double>(N3 - N2)));
+
+	std::fstream outputFile(tarFile, std::ios_base::out);
+	if (!outputFile.is_open()) {
+		std::cerr << "open output file error. at: " << tarFile << std::endl;
+		return false;
+	}
+
+	outputFile << "% src file: " << srcFileOne << "\n";
+	outputFile << "% src file: " << srcFileTwo << "\n";
+	outputFile << kendall << "\n";
+
+	outputFile.close();
 
 	return true;
 }
